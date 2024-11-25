61de86ae9fe2f91ed3923839238ccada603962ea
James Espichan Vilca <84110446+jamesev15@users.noreply.github.com>
2024-11-24 19:03:10 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from pymongo.mongo_client import MongoClient
 
 username = os.getenv("DB_USERNAME")
@@ -13,10 +14,18 @@ def get_gifts():
     collection = client["wedding"]["jjgifts"]
     return collection.find()
 
-def update_gift(state_key, status: bool):
-    collection = client["wedding"]["jjgifts"]
+@st.dialog("Confirma tu elección")
+def update_gift(item, state_key, status: bool):
+    st.write(f"Tu elección: {item}")
+    senders = st.text_input("Quién eligió este regalo?: ")
+    if st.button("Confirmar"):
+        collection = client["wedding"]["jjgifts"]
 
-    collection.update_one(
-        {"uuid": state_key},  # Filter
-        {"$set": {"reserved": status}}  # Update operation
-    )
+        collection.update_one(
+            {"uuid": state_key},  # Filter
+            {"$set": {"reserved": status}}  # Update operation
+        )
+        st.rerun()
+    elif st.button("Elegir otro regalo"):
+        st.session_state[state_key] = "Libre 🟢"
+        st.rerun()
